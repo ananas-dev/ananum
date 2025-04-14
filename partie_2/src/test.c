fd7a0db7599c44bbcde779e5a5927c79ea57959f
mystudentiskoro <henri.cornette@student.uclouvain.be>
2025-04-14 16:20:08 +0200

    dleet

--- partie_2/src/test.c
+++ partie_2/src/test.c
@@ -25,13 +25,38 @@ void test_Matvec1() {
     t_assert_double(Av[0], ==, 14.0);
     t_assert_double(Av[1], ==, 32.0);
     t_assert_double(Av[2], ==, 50.0);
-  }
+}
 
+//
+void test_CG1() {
+
+  int n = 3;
+  int nnz = 4;
+  double A[4] = {1.0, 1.0, 1.0};
+  int cols_idx[4] = {0, 1, 2};
+  int rows_idx[4] = {0, 1, 2, 3};
+  // A is the identity matrix
+
+  double v[3] = {1.0, 2.0, 3.0};
+  double Av[3] = {100101.0, 0.0,
+                  0.0}; // should be set as (0, 0, 0) at the beginning of CG
+
+  // int CG(int n, int nnz, double eps, double *A, int *cols, int *rows_idx,
+  //        double *b, double *x);
+
+  //int n,int nnz,const int *rows_idx,const int *cols,const double *A,const double *b,double *x, double eps)
+  CG(n,nnz, rows_idx, cols_idx, A, Av, v, 1.0);
+
+  t_assert_double(Av[0], ==, v[0]);
+  t_assert_double(Av[1], ==, v[1]);
+  t_assert_double(Av[2], ==, v[2]);
+}
   
 
 int main(int argc, char **argv) {
   testsuite_t *csr_suite = t_registerTestSuite("Matvec");
   t_addTestToSuite(csr_suite, "matvec CSR multiplication 1", test_Matvec1);
+  t_addTestToSuite(csr_suite, "CG test", test_CG1);
 
   return t_runSuites(argc, argv);
 }
